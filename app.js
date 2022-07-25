89535055d7066e247ec38d812d17c0df9af461d7
Julie Nisbet <jmnisbet@gmail.com>
2022-07-25 11:39:39 -0700

    adding list of dogs

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 
 const app = express();
 
-app.use("/benny", (req, res) => {
-  res.send("benny is cute!");
-});
+// app.use("/benny", (req, res) => {
+//   res.json({ name: "Benny", age: 7 });
+// });
+app.use("/dogs", require("./lib/controllers/dogs.js"));
 
 app.use("/", (req, res) => {
-  res.send("hello world!");
+  res.send("hello world! you look great today by the way!");
 });
 
 app.listen(7890, () => {
